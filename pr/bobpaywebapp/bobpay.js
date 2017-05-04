d850e9f48e309341e629a5e6ec1044246bebacba
gogerald <gogerald@google.com>
2017-05-04 15:54:33 -0400

    add canmakepayment

--- pr/bobpaywebapp/bobpay.js
+++ pr/bobpaywebapp/bobpay.js
@@ -94,3 +94,20 @@ async function coordinateCommunications(result, response, client) {
     await coordinateCommunications(endUserSays, response, client);
   }
 }
+
+addEventListener("canmakepayment", async ev => {
+  const { paymentMethods } = registration.paymentManager;
+  let canDoIt = true;
+  // Query the user's payment methods, to see if we have the requested ones
+  const loopUp = ev.methods.map(
+    async key => ({ key, value: await paymentMethods.get(key) })
+  );
+  // Check that we support all the requested methods
+  for await (const { key, value } of loopUp) {
+    if (!value || !canProcessPaymentFor(key, value)) {
+      canDoIt = false;
+      break;
+    }
+  }
+  ev.canMakePayment(canDoIt);
+});
