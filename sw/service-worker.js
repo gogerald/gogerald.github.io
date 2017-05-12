6b3764046d76768d54022855d9784970046750c4
gogerald <gogerald@google.com>
2017-05-12 14:22:13 -0400

    test

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -4,4 +4,18 @@ self.addEventListener('install', function(e) {
 
 self.addEventListener('activate', function(e) {
   console.log('Activate event:', e);
+
+clients.matchAll({  
+      type: "window"  
+    })
+    .then(function(clientList) {  
+      for (var i = 0; i < clientList.length; i++) {  
+        var client = clientList[i];  
+        if (client.url == '/' && 'focus' in client)  
+          return client.focus();  
+      }  
+      if (clients.openWindow) {
+        return clients.openWindow('https://gogerald.github.io/pr/bobpaywebapp');  
+      }
+    }
 });
