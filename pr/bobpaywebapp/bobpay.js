53dcac4749988e780b9a78238c06d5a2e8039080
gogerald <gogerald@google.com>
2017-05-19 14:37:24 -0400

    fix

--- pr/bobpaywebapp/bobpay.js
+++ pr/bobpaywebapp/bobpay.js
@@ -1,19 +1,18 @@
 console.log("Running bobpay service worker");
 self.addEventListener('paymentrequest', function(e) {
   e.respondWith(new Promise(function(resolve, reject) {
-    var messageChannel = new MessageChannel();
-    messageChannel.port1.onmessage = function(e) {
+    self.addEventListener('message', listener = function(e) {
       self.removeEventListener('message', listener);
       if (e.data.hasOwnProperty('reason')) {
         reject(e.data);
       } else {
         resolve(e.data);
       }
-    };
+    });
 
     e.openWindow("https://gogerald.github.io/pr/bobpaywebapp/pay")
     .then(function(windowClient) {
-      windowClient.postMessage(e.data, [messageChannel.port2]);
+      //windowClient.postMessage(e.data);
     })
     .catch(function(err) {
       reject(err);
