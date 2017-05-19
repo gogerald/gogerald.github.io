9e39e947a55fdbc274c12ff137c95e1c9c97d071
gogerald <gogerald@google.com>
2017-05-19 14:49:06 -0400

    test

--- pr/bobpaywebapp/bobpay.js
+++ pr/bobpaywebapp/bobpay.js
@@ -3,11 +3,11 @@ self.addEventListener('paymentrequest', function(e) {
   e.respondWith(new Promise(function(resolve, reject) {
     self.addEventListener('message', listener = function(e) {
       self.removeEventListener('message', listener);
-      if (e.data.hasOwnProperty('reason')) {
-        reject(e.data);
-      } else {
+      //if (e.data.hasOwnProperty('reason')) {
+      //  reject(e.data);
+     // } else {
         resolve(e.data);
-      }
+      //}
     });
 
     e.openWindow("https://gogerald.github.io/pr/bobpaywebapp/pay")
