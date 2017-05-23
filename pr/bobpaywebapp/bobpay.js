26ea71a50fb70438c4d765af89aad84b73b572f1
gogerald <gogerald@google.com>
2017-05-23 16:13:27 -0400

    fix test

--- pr/bobpaywebapp/bobpay.js
+++ pr/bobpaywebapp/bobpay.js
@@ -1,18 +1,35 @@
 console.log("Running bobpay service worker");
 self.addEventListener('paymentrequest', function(e) {
   e.respondWith(new Promise(function(resolve, reject) {
+    let payment_app_window = undefined;
+    let window_ready = false;
+    let payment_request_event = e.data;
+
+    let maybeSendPaymentRequest = function() {
+      if (payment_app_window && window_ready)
+        payment_app_window.postMessage(payment_request_event);
+    };
+
     self.addEventListener('message', listener = function(e) {
+
+      if (e.data == "payment_app_window_ready") {
+        window_ready = true;
+        maybeSendPaymentRequest();
+        return;
+      }
+
       self.removeEventListener('message', listener);
-      //if (e.data.hasOwnProperty('reason')) {
-      //  reject(e.data);
-     // } else {
+      if(e.data.methodName) {
         resolve(e.data);
-      //}
+      } else {
+        reject(e.data);
+      }
     });
 
     e.openWindow("https://gogerald.github.io/pr/bobpaywebapp/pay")
-    .then(function(windowClient) {
-      //windowClient.postMessage(e.data);
+    .then(window_client => {
+      payment_app_window = window_client;
+      maybeSendPaymentRequest();
     })
     .catch(function(err) {
       reject(err);
