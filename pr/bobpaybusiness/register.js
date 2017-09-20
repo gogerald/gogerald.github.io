91539b4039bcf2e769f67c3f9a19346a870738ee
gogerald <gogerald@google.com>
2017-09-20 17:23:11 -0400

    add capabilities

--- pr/bobpaybusiness/register.js
+++ pr/bobpaybusiness/register.js
@@ -28,6 +28,26 @@ function addInstruments(registration) {
         ],
         enabledMethods: ["https://emerald-eon.appspot.com/bobpay"]
       }),
+    registration.paymentManager.instruments.set(
+      "c8126178-3bba-4d09-8f00-0771bcfd3b12",
+      {
+        name: "My visa card 4012 *** *** *** 1881",
+        icons: [{
+          src:"images/nonexistfortext.png",
+          sizes:"32x32",
+          type:"image/png"
+        },
+          {
+          src:"images/visa.png",
+          sizes:"32x32",
+          type:"image/png"}
+        ],
+        enabledMethods: ["basic-card"],
+        capabilities: {
+          supportedNetworks: ['visa'],
+          supportedTypes: ['credit']
+        }
+      }),
     ];
 
     return Promise.all(instrumentPromises);
