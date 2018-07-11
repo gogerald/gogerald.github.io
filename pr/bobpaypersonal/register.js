9b6c36406beadbfa65e40437ea031ecff722dc37
gogerald <ganggui.tang@dal.ca>
2018-07-11 11:48:58 -0400

    Update register.js

--- pr/bobpaypersonal/register.js
+++ pr/bobpaypersonal/register.js
@@ -21,31 +21,11 @@ function addInstruments(registration) {
       {
         name: "My Bob Pay Account: gogerald@google.com",
         icons: [{
-          src:"images/nonexistfortext.png",
-          sizes:"32x32",
-          type:"image/png"
-        },
-          {
           src:"images/bobpay.png",
           sizes:"32x32",
           type:"image/png"}
         ],
         enabledMethods: ["https://emerald-eon.appspot.com/bobpay"]
-      }),
-    registration.paymentManager.instruments.set(
-      '12345',
-      {
-        name: "Visa ****1111",
-        icons: [{
-          src: "images/visa.png",
-          sizes: '32x32',
-          type: 'image/png'
-        }],
-        enabledMethods: ["basic-card"],
-        capabilities: {
-          supportedNetworks: ["visa"],
-          supportedTypes: ["credit"]
-        }
       }),
     ];
 
